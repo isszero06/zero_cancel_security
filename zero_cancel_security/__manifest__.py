bd1c4b9f24e4c9201a7b70848a37ba242495c1f0
isszero06 <76986925+isszero06@users.noreply.github.com>
2023-09-07 05:36:09 +0300

    Add files via upload

---
+++ zero_cancel_security/__manifest__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Cancel and Reset to Draft Buttons Control',
+    'version': '16.0.1',
+    'category': 'Accounting',
+    "author": 'Zero Systems',
+    "company": 'Zero for Information Systems',
+    "website": "https://www.erpzero.com",
+    "email": "sales@erpzero.com",
+    "sequence": 0,
+
+    'summary': """Cancel and Reset to Draft Buttons Control""",
+
+    'description': """
+            Sales Order Cancel and Reset to Draft Buttons Security.
+            Purchase Order Cancel and Reset to Draft Buttons Security.
+            Stock Picking Cancel and Reset to Draft Buttons Security.
+            All Account Move Cancel and Reset to Draft Buttons Security.
+            All Payments Cancel and Reset to Draft Buttons Security.
+            """,
+    'depends': [
+        'base','purchase','sale','account','stock'
+    ],
+    'data': [
+        'security/security.xml',
+        'views/view.xml',
+    ],
+    'license': 'OPL-1',
+    'live_test_url': 'https://youtu.be/0W78Xozfbjs',
+    'images': ['static/description/cancel_block.png'],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'price': 00.0,
+    'currency': 'EUR',
+    'pre_init_check_vers': 'pre_init_check_vers',
+}
