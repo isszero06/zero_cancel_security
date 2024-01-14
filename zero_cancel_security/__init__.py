d36190af3391373bde06603b7a841e6c377d2244
Zero Systems <isszero06@gmail.com>
2024-01-14 22:41:24 +0200

    Update __init__.py

--- zero_cancel_security/__init__.py
+++ zero_cancel_security/__init__.py
@@ -3,21 +3,12 @@
 # Author      : Zero For Information Systems (<www.erpzero.com>)
 # Copyright(c): 2016-Zero For Information Systems
 # All Rights Reserved.
-#
+#Zerosystems #odoo #erp
 # This program is copyright property of the author mentioned above.
 # You can`t redistribute it and/or modify it.
 #
 #################################################################################
 
-from odoo import SUPERUSER_ID, api
 
 
-def pre_init_check_vers(cr):
-    from odoo.service import common
-    from odoo.exceptions import Warning
-    version_info = common.exp_version()
-    server_serie =version_info.get('server_serie')
-    if server_serie!='17.0':
-        raise Warning('Module support Odoo series 17.0 found {}.'.format(server_serie))
-    return True
 
